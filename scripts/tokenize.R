26b6f8aed09cb62b65628782d1c1ad4a933ba165
bcritt1 <122565439+bcritt1@users.noreply.github.com>
2023-02-09 12:36:06 -0800

    Add files via upload

---
+++ scripts/tokenize.R
@@ -0,0 +1,26 @@
+install.packages("tokenizers")
+install.packages("tidyverse")
+install.packages("tidytext")
+
+library(tokenizers)
+library(tidyverse)
+library(tidytext)
+
+input_loc <- "/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson"
+files <- dir(input_loc, full.names = TRUE)
+text <- c()
+for (f in files) {
+  text <- c(text, paste(readLines(f), collapse = "\n"))
+}
+
+#tokenize with tokenizers
+words <- tokenize_words(text)
+
+#adds file names, but puts every word as a row
+#words2 <- do.call(rbind, Map(data.frame, work=files, text=words))
+
+#tokenize with tidytext: also puts every word as own row. Can connect to file 
+#names with simple merge of numbered "files" list
+text2 <- enframe(text)
+tidyText <- text2 %>%
+  unnest_tokens(word, value)
