2efd841259a0f2dbbd902b27c3ebc8f70bc232e2
bcritt1 <bcritt@stanford.edu>
2023-03-13 14:31:29 -0700

    add r

---
+++ scripts/tokenizers/tokenize.r
@@ -0,0 +1,25 @@
+install.packages("tokenizers")
+install.packages("tidyverse")
+
+library(tokenizers)
+library(tidyverse)
+
+
+
+input_loc <- "/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson"
+files <- dir(input_loc, full.names = TRUE)
+text <- c()
+for (f in files) {
+  text <- c(text, paste(readLines(f), collapse = "\n"))
+}
+
+#tokenize to list of tokens, no metadata
+tokens <- tokenize_words(text)
+
+#create df with file names and tokenize text with tokenizers
+test <- cbind(files,text)
+df <- data.frame(test)
+df$tokens <- tokenize_words(df[,2])
+
+write.csv(df, "data.csv")
+
