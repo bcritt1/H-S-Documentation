6b747dff490bf974a845e7305a8459b8fb30ff0a
bcritt1 <bcritt@stanford.edu>
2023-03-21 11:26:30 -0700

    word2vec files

--- scripts/word_embedding/word2vec.R
+++ scripts/word_embedding/word2vec.R
@@ -3,6 +3,12 @@
 install.packages("word2vec")
 library(word2vec)
 #set.seed(123456789)
+input_loc <- "/home/users/bcritt/corpus/"
+files <- dir(input_loc, full.names = TRUE)
+text <- c()
+for (f in files) {
+  text <- c(text, paste(readLines(f), collapse = "\n"))
+}
 #create model, type can be cbow or skip-gram, dim is number of coordinates, iterations is number of trainings
 model <- word2vec(x = text, type = "skip-gram", dim = 15, iter = 20) 
 embedding <- as.matrix(model)
