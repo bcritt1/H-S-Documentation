583ebafa4715c89f53ffd0a2d0942fb42477debe
bcritt1 <bcritt@stanford.edu>
2023-04-04 14:52:37 -0700

    edit

--- scripts/geocoding/python/geocode.py
+++ scripts/geocoding/python/geocode.py
@@ -17,42 +17,43 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
-# Read in a directory of txt files as the corpus using the os library.
+# Read in a directory of txt files as the corpus using the os library. It's most efficient to locate this in scratch. My scratch path is below: you will need to adjust to reflect your path.
 
 import os
-corpusdir = '/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson/'
+corpusdir = '/scratch/users/bcritt/corpus/'
 corpus = []
 for infile in os.listdir(corpusdir):
     with open(corpusdir+infile, errors='ignore') as fin:
         corpus.append(fin.read())
 
-#word tokenize, pos tag, and ner tag corpus
+# NLTK implementation of tokenize, pos tag, and ner tag corpus. Not used in current workflow.
 
-ne_tree = nltk.ne_chunk(pos_tag(word_tokenize(corpus[0])))
-print(ne_tree)
+#ne_tree = nltk.ne_chunk(pos_tag(word_tokenize(corpus[0])))
+#print(ne_tree)
 
 #optional tree illustration of NER
 
 #!pip3 install svgling
 #ne_tree
 
-"""
 #spacy implementation of NER
 !pip3 install spacy && python3 -m spacy download en_core_web_sm
 import spacy 
 nlp = spacy.load("en_core_web_sm")
 
 doc = nlp(corpus[0])
+# Perform NER on docs, outputing the tokens and entity types
 ner_output = []
 for token in doc:
-    ner_output.append(token.text + token.ent_type_
+    ner_output.append(token.text + token.ent_type_)
 
+# Filter NER data to only include things labeled as "GPE" (geopolitical entities)
 df = pd.DataFrame(ner_output)
 places = df.loc[df[0].str.contains("GPE")]
 places[0] = places[0].str.replace('GPE','')
 """
 
-#geocoding GPE Data
+# Geocode GPE Data with Nominatum
 
 !pip3 install urllib
 !pip3 install requests
@@ -68,4 +69,6 @@ def geocode2(locality):
         return('-1')
 
 places['geocoded'] = places[0].apply(geocode2)
-places.to_csv('places.csv')
+
+# Write out data to csv in scratch for efficiency.
+places.to_csv('/scratch/users/bcritt/output/places.csv')
