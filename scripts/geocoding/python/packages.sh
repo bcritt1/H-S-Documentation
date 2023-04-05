917df8e0d25b94ea1a7917f1759e935375e7badd
bcritt1 <bcritt@stanford.edu>
2023-04-05 09:40:26 -0700

    added repo pull to sh file

--- scripts/geocoding/python/packages.sh
+++ scripts/geocoding/python/packages.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+ml system subversion/1.12.2
+svn export https://github.com/bcritt1/H-S-Documentation/trunk/scripts/geocoding/python/ geocoding
 ml python/3.9.0
-pip3 install --user -r requirements.txt
+pip3 install --user -r ./geocoding/requirements.txt
 python3 -m spacy download en_core_web_sm
