b71054ca9cf2e9ab10acf34db014b5b950d9a553
bcritt1 <bcritt@stanford.edu>
2023-04-10 10:45:06 -0700

    change shell script

--- scripts/text_cleaning/packages.sh
+++ scripts/text_cleaning/packages.sh
@@ -1,3 +1,6 @@
-ml python/3.9.0
-pip3 install --user nltk
-python3 -m ntlk.downloader all
+#!/bin/bash
+
+ml load python/3.9.0
+pip3 install nltk
+pip install --upgrade certifi
+python3 -m nltk.downloader all
