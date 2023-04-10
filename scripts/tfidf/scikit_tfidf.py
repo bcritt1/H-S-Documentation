0eef7d6e592114123505fd21d81e3c02bbfa6485
bcritt1 <bcritt@stanford.edu>
2023-04-10 11:12:55 -0700

    sherlock runs

--- scripts/tfidf/scikit_tfidf.py
+++ scripts/tfidf/scikit_tfidf.py
@@ -1,9 +1,11 @@
 Adapted from https://programminghistorian.org/en/lessons/analyzing-documents-with-tfidf
 
 from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
 all_txt_files =[]
-for file in Path("/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson/").rglob("*.txt"):
+for file in Path("/scratch/users/bcritt/corpus/").rglob("*.txt"):
      all_txt_files.append(file.parent / file.name)
 # counts the length of the list
 n_files = len(all_txt_files)
@@ -15,21 +17,17 @@ for txt_file in all_txt_files:
         txt_file_as_string = f.read()
     all_docs.append(txt_file_as_string)
 
-#import the TfidfVectorizer from Scikit-Learn.
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 vectorizer = TfidfVectorizer(max_df=.65, min_df=1, stop_words=None, use_idf=True, norm=None)
 transformed_documents = vectorizer.fit_transform(all_docs)
 
 transformed_documents_as_array = transformed_documents.toarray()
 
-import pandas as pd
-
 # make the output folder if it doesn't already exist
-Path("/Users/bcritt/Documents/StanfordProjects/Scripts/sherlockScripts/tfidf/tf_idf_output").mkdir(parents=True, exist_ok=True)
+Path("/scratch/users/bcritt/outputs/tf_idf_output").mkdir(parents=True, exist_ok=True)
 
 # construct a list of output file paths using the previous list of text files the relative path for tf_idf_output
-output_filenames = [str(txt_file).replace(".txt", ".csv").replace("/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson/", "/Users/bcritt/Documents/StanfordProjects/Scripts/sherlockScripts/tf_idf/tf_idf_output/") for txt_file in all_txt_files]
+output_filenames = [str(txt_file).replace(".txt", ".csv").replace("/scratch/users/bcritt/corpus/", 
+"/scratch/users/bcritt/outputs/tf_idf_output/") for txt_file in all_txt_files]
 
 # loop each item in transformed_documents_as_array, using enumerate to keep track of the current position
 for counter, doc in enumerate(transformed_documents_as_array):
