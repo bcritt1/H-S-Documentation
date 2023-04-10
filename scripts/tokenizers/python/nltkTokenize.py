07eadc091fac8de418e402fb912612f77f86691d
bcritt1 <bcritt@stanford.edu>
2023-04-10 15:33:15 -0700

    final files

--- scripts/tokenizers/python/nltkTokenize.py
+++ scripts/tokenizers/python/nltkTokenize.py
@@ -1,13 +1,14 @@
 import nltk
 import ssl
+import re
 import os
 import pandas as pd
 from nltk.tokenize import sent_tokenize
-from nltk.tokenize.treebank import TreebankWordTokenizer
 nltk.download('punkt')
 
 
-# This may or may not be necessary for you. Gives python permission to access the internet so we can download libraries.
+# This may or may not be necessary for you. Gives python permission to access the internet so we can download 
+libraries.
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
@@ -22,19 +23,19 @@ for infile in os.listdir(corpusdir):
     with open(corpusdir+infile, errors='ignore') as fin:
         corpus.append(fin.read())
 
-# Function to tokenize the corpus into sentences and then into words, maintaining sentence-level information. This format is required for gensim word embedding downstream
-def make_sentences(list_txt):
-    all_txt = []
-    for txt in list_txt:
-        lower_txt = txt.lower()
-        sentences = sent_tokenize(lower_txt)
-        sentences = [tokenizer.tokenize(sent) for sent in sentences]
-        all_txt += sentences
-        print(len(sentences))  
-    return all_txt
+# convert corpus to string instead of list
+sorpus = str(corpus)
+
+# this particular corpus has a multitude of "\n's" due to its original encoding. This removes them; code can be 
+modified to remove other text artifacts before tokenizing.
+
+sorpus = re.sub(r'(\\n[ \t]*)+', '', sorpus)
+
+# could also split into words (or paragraphs, etc.)
+#words = word_tokenize(sorpus)
 
 # Call the function
-sentences = make_sentences(corpus)
+sentences = sent_tokenize(sorpus)
 
 df = pd.DataFrame(sentences)
 df.to_csv('/scratch/users/bcritt/outputs/sentences.csv')
